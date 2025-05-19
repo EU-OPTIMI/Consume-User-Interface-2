c1a2cfee6763a983cb81454506090f771e21599e
Ryan Ford <ryan.kford@icloud.com>
2025-05-19 11:17:39 +0300

    URLS

--- consume/urls.py
+++ consume/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import connector_offers, selected_offer, consume_offer
 
 urlpatterns = [
-    path('connector_offers/', connector_offers, name='connector_offers'),
-    path('selected_offer/<str:offer_id>/', selected_offer, name='selected_offer'),
-    path('consume_offer/<path:offer_id>/', consume_offer, name='consume_offer'),  # Allow slashes in offer_id
+    path('consume/connector_offers/', connector_offers, name='connector_offers'),
+    path('consume/selected_offer/<str:offer_id>/', selected_offer, name='selected_offer'),
+    path('consume/consume_offer/<path:offer_id>/', consume_offer, name='consume_offer'),  # Allow slashes in offer_id
 ]
