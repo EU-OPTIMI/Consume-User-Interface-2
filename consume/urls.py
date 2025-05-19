35846a3a28cadec160bf418b9d855c2cfa140f50
Ryan Ford <ryan.kford@icloud.com>
2025-05-19 11:33:04 +0300

    More

--- consume/urls.py
+++ consume/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     # BEFORE: path('consume/connector_offers/', …)
     # AFTER:
     path('connector_offers/', connector_offers, name='connector_offers'),
-    path('selected_offer/<str:offer_id>/', selected_offer, name='selected_offer'),
+    path('consume/selected_offer/<str:offer_id>/', selected_offer, name='selected_offer'),
     path('consume_offer/<path:offer_id>/', consume_offer, name='consume_offer'),
 ]
